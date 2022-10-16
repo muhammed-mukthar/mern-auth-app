ca0756c736bd22e5fd9624a080742838c7744018
Muhammed Mukthar <muktharmanaf28@gmail.com>
2022-10-16 15:39:00 +0530

    user register added

---
+++ Servers/routes/user.js
@@ -0,0 +1,24 @@
+const router=require("express").Router()
+const bcrypt=require('bcrypt')
+const {User,validate}=require('../models/user')
+
+
+router.post("/",async(req,res)=>{
+    try{
+        const {error}=validate(req.body);
+        if(error)
+            return res.status(400).send({message:error.details[0].message});
+        const user=await User.findOne({email:req.body.email})
+        if(user)
+            return res.status(409).send({message:"user with given email already exists"})
+            const salt=await bcrypt.genSalt(Number(process.env.SALT))
+            const hashPassword=await bcrypt.hash(req.body.password,salt)
+            await new User({...req.body,password:hashPassword}).save()
+            res.status(201).send({message:"User created successfuly"})
+
+    }catch(err){
+        res.status(500).send('Internal server error')
+
+
+    }
+})
