a30c7c683f2160f8ee7528770df732438a3e2d33
Muhammed Mukthar <muktharmanaf28@gmail.com>
2022-10-16 15:26:05 +0530

    user schema added

--- Servers/index.js
+++ Servers/index.js
@@ -2,11 +2,15 @@ require('dotenv').config()
 const express=require('express')
 const app=express()
 const cors=require('cors')
-
+const connection=require('./db')
 /* ------------------------------- middleware ------------------------------- */
 
 app.use(express.json())
 app.use(cors())
+
+
+/* ------------------------------ database connect ------------------------------ */
+connection()
 
 
 const port=process.env.PORT||5000
